280d91cd0ddee2b30d44cdccd4f3035cbcc8fdcd
Chong Sun <sonshicyu@gmail.com>
2018-11-04 01:36:10 -0500

    Add function to get apple data for constructing SVM

--- getData.py
+++ getData.py
@@ -16,14 +16,33 @@ djia = []
 with open("djia", 'r') as f:
     for line in f:
         djia.append(float(line.rstrip()))
-djia = [(i-j)/j for i,j in zip(djia, djia[1:])]
+djia = [(j-i)/i for i,j in zip(djia, djia[1:])]
 
 sp = []
 with open('sp', 'r') as f:
     for line in f:
         item = line.rstrip().split(',')
         sp.append(float(item[-2]))
-sp = [(i-j)/j for i,j in zip(sp, sp[1:])]
+sp = [(j-i)/i for i,j in zip(sp, sp[1:])]
 
 clusterData = np.array([unemployment, inflation, djia, sp]).T
 np.savetxt('data/clusterData.txt', clusterData)
+
+
+def getStockPrice(fileName):
+    price = []
+    size = []
+    with open(fileName) as f:
+        for line in f:
+            item = line.rstrip().split(',')
+            price.append(float(item[-2]))
+            size.append(float(item[-1]))
+    return price, size[2:]
+
+applePrice, appleSize = getStockPrice("data/apple")
+appleThreeMonthMA = [(i+j+k)/3 for i,j,k in zip(applePrice, applePrice[1:], applePrice[2:])]
+appleTwoMonthMA = [(i+j)/2 for i,j in zip(applePrice[1:], applePrice[2:])]
+appleReturn = [(j-i)/i for i,j in zip(applePrice[1:], applePrice[2:])]
+
+appleData = np.array([appleReturn, appleTwoMonthMA, appleThreeMonthMA, appleSize]).T
+np.savetxt('data/appleTrainData.txt', appleData)
