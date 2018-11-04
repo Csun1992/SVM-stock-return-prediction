92f8aa8813317efee39a5c5b6b473b0f9a9d028f
Chong Sun <sonshicyu@gmail.com>
2018-11-04 03:23:08 -0600

    Change getData.py file to deal with the reorganizaiton of data files

--- getData.py
+++ getData.py
@@ -1,25 +1,25 @@
 import numpy as np
 
 unemployment = []
-with open("unemployment", 'r') as f:
+with open("data/unemployment", 'r') as f:
    for line in f:
        unemployment.append(map(float, line.rstrip().rstrip('\n').split(' ')))
 unemployment = [item for sublist in unemployment for item in sublist]
 
 inflation = []
-with open("inflation", 'r') as f:
+with open("data/inflation", 'r') as f:
     for line in f:
         inflation.append(line.rstrip().rstrip('\n').split('   '))
 inflation = map(float, [item.rstrip('%') for sublist in inflation for item in sublist])
 
 djia = []
-with open("djia", 'r') as f:
+with open("data/djia", 'r') as f:
     for line in f:
         djia.append(float(line.rstrip()))
 djia = [(j-i)/i for i,j in zip(djia, djia[1:])]
 
 sp = []
-with open('sp', 'r') as f:
+with open('data/sp', 'r') as f:
     for line in f:
         item = line.rstrip().split(',')
         sp.append(float(item[-2]))
