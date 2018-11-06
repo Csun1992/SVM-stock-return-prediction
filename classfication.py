8d746fa163bdbdb79d43464f108308570a6375b9
Csun1992 <sonshicyu@gmail.com>
2018-11-05 20:41:58 -0600

    Create a "stockPrediction" class for further experiments

--- classfication.py
+++ classfication.py
@@ -1,59 +1,54 @@
 import numpy as np
+from sys import exit
 from sklearn import preprocessing, cluster, model_selection, svm
 
-# Clustering step
-# Cluster the months from 01/01/1990 - 09/01/2018 into 3 clusters
-# based on the macroeconomic factors
-clusterData = np.loadtxt('data/clusterData.txt')
-cleanData = preprocessing.scale(clusterData)
+class stockPrediction:
+    def __init__(self, stockDataLoc, clusterNum=3, fold=10, clusterDataLoc="data/clusterData.txt"):
+        self.stockDataLoc = stockDataLoc
+        self.clusterNum = clusterNum
+        self.clusterDataLoc = clusterDataLoc
+        self.fold = fold
+        self.group, self.label = [], []
+        self.scores = []
 
-kmeans = cluster.KMeans(n_clusters=3, random_state=11).fit(cleanData)
+    def cluster(self):
+        data = np.loadtxt(self.clusterDataLoc)
+        cleanData = preprocessing.scale(data)
+        kmeans = cluster.KMeans(n_clusters=self.clusterNum, random_state=11).fit(cleanData)
+        groupNum = np.array(kmeans.labels_)
+        return groupNum
 
-labels = np.array(kmeans.labels_)
-
-appleData = np.loadtxt('data/appleTrainData.txt') 
-aggApple = appleData[:, :-1]
-aggResult = appleData[:, -1]
-
-# Split the apple idiosycratic data into 3 clusters
-group1 = appleData[labels==0, :-1]
-result1 = appleData[labels==0, -1]
-group2 = appleData[labels==1, :-1]
-result2 = appleData[labels==1, -1]
-group3 = appleData[labels==2, :-1]
-result3 = appleData[labels==2, -1]
-
-# Build a svm for each cluster 
-# Use 5-fold cross-validation to estimate the out-of-sample error
-fold = 5 
-aggClf = svm.SVC()
-clf = [svm.SVC() for i in range(4)]
-aggScore = model_selection.cross_val_score(aggClf, aggApple, aggResult, cv=fold)
-scores1 = model_selection.cross_val_score(clf[0], group1, result1, cv=fold)
-scores2 = model_selection.cross_val_score(clf[1], group2, result2, cv=fold)
-scores3 = model_selection.cross_val_score(clf[2], group3, result3, cv=fold)
+    def clusterStockPrice(self):
+        groupNum = self.cluster()
+        data = np.loadtxt(self.stockDataLoc)
+        for i in range(self.clusterNum):
+            self.group.append(data[groupNum==i, :-1])
+            self.label.append(data[groupNum==i, -1])
+        return (self.group, self.label)
+            
+    def svmFitting(self):
+        clf = [svm.SVC() for i in range(self.clusterNum)]
+        for i in range(self.clusterNum):
+            self.scores.append(model_selection.cross_val_score(clf[i], self.group[i], self.label[i],
+                    cv=self.fold))
+        return self.scores 
     
-# Report the result
-print aggScore.mean()    
-print aggScore.std()
-print '\n'
+    def run(self):
+        self.clusterStockPrice()
+        self.svmFitting()
+        return self.scores
 
-print scores1.mean()
-print scores1.std()
-print '\n' 
-print scores2.mean()
-print scores2.std()
-print '\n' 
+    def reportResult(self):
+        for i in range(self.clusterNum):
+            print "group NO." + str(i+1) + " correct rate"
+            print self.scores[i].mean()
+            print "\n"
+        return self.scores
+    
+        
+apple = stockPrediction("data/appleTrainData.txt")
+apple.run()
+apple.reportResult()
+    
 
-print scores3.mean()
-print scores3.std()
-print '\n' 
-
-
-print '\n'    
-print np.size(result1)
-print np.size(result2)
-print np.size(result3)
-
-
-
+    
