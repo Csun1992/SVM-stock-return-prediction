d392b11c0e569941e71cb104ec1db3a8fd806215
Csun1992 <sonshicyu@gmail.com>
2018-11-05 21:55:44 -0600

    Add a method to train the model with full data set

--- classfication.py
+++ classfication.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sys import exit
 from sklearn import preprocessing, cluster, model_selection, svm
 
 class stockPrediction:
@@ -10,6 +9,7 @@ class stockPrediction:
         self.fold = fold
         self.group, self.label = [], []
         self.scores = []
+        self.clf = [svm.SVC() for i in range(self.clusterNum)]
 
     def cluster(self):
         data = np.loadtxt(self.clusterDataLoc)
@@ -26,13 +26,17 @@ class stockPrediction:
             self.label.append(data[groupNum==i, -1])
         return (self.group, self.label)
             
-    def svmFitting(self):
-        clf = [svm.SVC() for i in range(self.clusterNum)]
+    def crossValidation(self):
         for i in range(self.clusterNum):
-            self.scores.append(model_selection.cross_val_score(clf[i], self.group[i], self.label[i],
+            self.scores.append(model_selection.cross_val_score(self.clf[i], self.group[i], self.label[i],
                     cv=self.fold))
         return self.scores 
     
+    def train(self):
+        for i in range(self.clusterNum):
+            self.clf[i].fit(self.group[i], self.label[i])
+        return self.clf
+
     def findBaseRate(self):
         self.baseRate = []
         for i in range(self.clusterNum):
@@ -43,7 +47,8 @@ class stockPrediction:
     
     def run(self):
         self.clusterStockPrice()
-        self.svmFitting()
+        self.crossValidation()
+        self.train()
         self.findBaseRate()
         return self.scores
 
@@ -57,9 +62,17 @@ class stockPrediction:
         return self.scores
     
         
+# for the case when cluster = 3
 apple = stockPrediction("data/appleTrainData.txt")
 apple.run()
 apple.reportResult()
     
-
-    
+# Case when 2 clusters
+apple = stockPrediction("data/appleTrainData.txt", clusterNum=2)
+apple.run()
+apple.reportResult()
+   
+# Case when 4 clusters
+apple = stockPrediction("data/appleTrainData.txt", clusterNum=4)
+apple.run()
+apple.reportResult()
