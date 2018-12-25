9be91117e8681ca5a12f0dde1e16276abb89cb1c
Chong Sun <sonshicyu@gmail.com>
2018-12-25 14:50:42 -0600

    Built an virtual class classifier so svm can inherit from

---
+++ classifier.py
@@ -0,0 +1,60 @@
+import numpy as np
+from sklearn import preprocessing, cluster, model_selection, svm
+
+# This is a virtual class of classifier
+# Other classifiers will inherit from this class and rewrite the train() method
+
+class classifier(object):
+    def __init__(self, microDataLoc, clusterNum=1, macroDataLoc="data/clusterData.txt"):
+        self.microDataLoc = microDataLoc
+        self.macroDataLoc = macroDataLoc
+        self.clusterNum = clusterNum
+
+    def cluster(self):
+        data = np.loadtxt(self.macroDataLoc)
+        cleanData = preprocessing.scale(data)
+        kmeans = cluster.KMeans(n_clusters=self.clusterNum, random_state=11).fit(cleanData)
+        groupNum = np.array(kmeans.labels_)
+        return groupNum
+
+    def prepareData(self):
+        data = np.loadtxt(self.microDataLoc)
+        groupNum = self.cluster()
+        group, label = [], []
+        for i in range(self.clusterNum):
+            group.append(data[groupNum==i, :-1])
+            label.append(data[groupNum==i, -1])
+        return (group, label)
+
+    def trainTestSplit(self):
+        train, test, trainLabel, testLabel = [], [], [], []
+        group, label = self.prepareData()
+        for i in range(self.clusterNum):
+            trainData, testData, trainLabelData, testLabelData = model_selection.train_test_split(group[i],
+                    label[i], test_size=0.3, random_state=11)
+            train.append(trainData)
+            test.append(testData)
+            trainLabel.append(trainLabelData)
+            testLabel.append(testLabelData)
+        return (train, test, trainLabel, testLabel)
+            
+    def train(self):
+        pass        # virtual method to be overwritten in each specific classifier
+                    # must return a tuple of classifier instances, test data and test labels
+
+    def test(self):
+        clf, test, testLabel = self.train()
+        error = []
+        for i in range(self.clusterNum):
+            pred = (clf[i].predict(test[i]) == 1)
+            caseError = sum([i != j for (i,j) in zip(testLabel[i], pred)])
+            error.append(float(caseError)/len(pred))
+        return error
+
+    def reportResult(self):
+        error = self.test()
+        for i in range(self.clusterNum):
+            print "group NO." + str(i+1) + " correct rate"
+            print 1-error[i]
+        print '\n'
+        return error
--- svm.py
+++ svm.py
@@ -1,41 +1,12 @@
 import numpy as np
 from sklearn import preprocessing, cluster, model_selection, svm
+from classifier import classifier
 
 # Object that for svm with clustering 
-class StockPrediction(object):
+class svm(classifier):
     def __init__(self, microDataLoc, clusterNum=1, macroDataLoc="data/clusterData.txt"):
-        self.microDataLoc = microDataLoc
-        self.macroDataLoc = macroDataLoc
-        self.clusterNum = clusterNum
+        classifier.__init__(self, microDataLoc, clusterNum=1, macroDataLoc="data/clusterData.txt")
 
-    def cluster(self):
-        data = np.loadtxt(self.macroDataLoc)
-        cleanData = preprocessing.scale(data)
-        kmeans = cluster.KMeans(n_clusters=self.clusterNum, random_state=11).fit(cleanData)
-        groupNum = np.array(kmeans.labels_)
-        return groupNum
-
-    def prepareData(self):
-        data = np.loadtxt(self.microDataLoc)
-        groupNum = self.cluster()
-        group, label = [], []
-        for i in range(self.clusterNum):
-            group.append(data[groupNum==i, :-1])
-            label.append(data[groupNum==i, -1])
-        return (group, label)
-
-    def trainTestSplit(self):
-        train, test, trainLabel, testLabel = [], [], [], []
-        group, label = self.prepareData()
-        for i in range(self.clusterNum):
-            trainData, testData, trainLabelData, testLabelData = model_selection.train_test_split(group[i],
-                    label[i], test_size=0.3, random_state=11)
-            train.append(trainData)
-            test.append(testData)
-            trainLabel.append(trainLabelData)
-            testLabel.append(testLabelData)
-        return (train, test, trainLabel, testLabel)
-            
     def train(self):
         train, test, trainLabel, testLabel = self.trainTestSplit()
         clf = [svm.SVC() for i in range(self.clusterNum)]
@@ -44,30 +15,13 @@ class StockPrediction(object):
         return (clf, test, testLabel) # return test and testLabel to self.test() so no need to
                                       # recompute the testing data again
 
-    def test(self):
-        clf, test, testLabel = self.train()
-        error = []
-        for i in range(self.clusterNum):
-            pred = (clf[i].predict(test[i]) == 1)
-            caseError = sum([i != j for (i,j) in zip(testLabel[i], pred)])
-            error.append(float(caseError)/len(pred))
-        return error
-
-    def reportResult(self):
-        error = self.test()
-        for i in range(self.clusterNum):
-            print "group NO." + str(i+1) + " correct rate"
-            print 1-error[i]
-        print '\n'
-        return error
-
 
 # StockPredNoClassification class is a class to classify the stock price direction 
 # without using clustering. The macro data used for clustering now was combined with
 # micro data for classification
-class StockPredNoClustering(StockPrediction):
+class svmNoCluster(svm):
     def __init__(self, microDataLoc):
-        StockPrediction.__init__(self, microDataLoc)
+        svm.__init__(self, microDataLoc)
 
     def prepareData(self):
         group, label = [], []
@@ -90,43 +44,43 @@ class StockPredNoClustering(StockPrediction):
 if __name__ == "__main__":
 
     # without clustering
-    apple = StockPredNoClustering("data/appleTrainData.txt")
+    apple = svmNoCluster("data/appleTrainData.txt")
     apple.reportResult()
 
     # for the case when cluster = 3
-    apple = StockPrediction("data/appleTrainData.txt", clusterNum=3)
+    apple = svm("data/appleTrainData.txt", clusterNum=3)
     apple.reportResult()
         
     # Case when 2 clusters
-    apple = StockPrediction("data/appleTrainData.txt", clusterNum=2)
+    apple = svm("data/appleTrainData.txt", clusterNum=2)
     apple.reportResult()
        
     # Case when 4 clusters
-    apple = StockPrediction("data/appleTrainData.txt", clusterNum=4)
+    apple = svm("data/appleTrainData.txt", clusterNum=4)
     apple.reportResult()
 
     # Case when 1 cluster
-    apple = StockPrediction("data/appleTrainData.txt", clusterNum=1)
+    apple = svm("data/appleTrainData.txt", clusterNum=1)
     apple.reportResult()
 
 
     
     # without clustering
-    att = StockPredNoClustering("data/attTrainData.txt")
+    att = svm("data/attTrainData.txt")
     att.reportResult()
 
     # for the case when cluster = 3
-    att = StockPrediction("data/attTrainData.txt", clusterNum=3)
+    att = svm("data/attTrainData.txt", clusterNum=3)
     att.reportResult()
         
     # Case when 2 clusters
-    att = StockPrediction("data/attTrainData.txt", clusterNum=2)
+    att = svm("data/attTrainData.txt", clusterNum=2)
     att.reportResult()
        
     # Case when 4 clusters
-    att = StockPrediction("data/attTrainData.txt", clusterNum=4)
+    att = svm("data/attTrainData.txt", clusterNum=4)
     att.reportResult()
 
     # Case when 1 cluster
-    att = StockPrediction("data/attTrainData.txt", clusterNum=1)
+    att = svm("data/attTrainData.txt", clusterNum=1)
     att.reportResult()
