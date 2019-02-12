1475a2c233a41d63b89213621fed1091d3860fd7
Chong Sun <sonshicyu@gmail.com>
2019-02-11 23:00:28 -0600

    Add new getData file

---
+++ getData.py
@@ -0,0 +1,76 @@
+from sklearn import preprocessing, cluster, model_selection, svm
+import numpy as np
+import sys
+
+def getStockPrice(fileName):
+    price = []
+    size = []
+    with open(fileName) as f:
+        for line in f:
+            item = line.rstrip().split(',')
+            price.append(float(item[-2]))
+            size.append(float(item[-1]) / 1000000)
+    return price, size[2:-1]
+
+def getInputData(fileName):        
+    fileName = 'data/' + fileName
+    price, size = getStockPrice(fileName)
+    threeMonthMA = [(i+j+k)/3 for i,j,k in zip(price, price[1: ], price[2: ])]
+    del threeMonthMA[-1]
+    twoMonthMA = [(i+j)/2 for i,j in zip(price[1: ], price[2: ])]
+    del twoMonthMA[-1]
+    stockReturn = [(j-i)/i for i,j in zip(price[1: ], price[2: ])]
+    del stockReturn[-1]
+    classification = np.array(map(int, [i > 0.005 for i in stockReturn])).reshape(-1, 1)
+    inputData = np.array([price[2:-1], twoMonthMA, threeMonthMA]).T
+#   inputData = preprocessing.scale(inputData)
+    inputData = np.concatenate((inputData, classification), axis = 1)
+#    inputData = np.array([price[2:-1], twoMonthMA, threeMonthMA, classification]).T
+    fileName = fileName + 'TrainData.txt'
+    np.savetxt(fileName, inputData)
+    return inputData
+
+
+if __name__ == '__main__':
+    capacity = []
+    with open("data/capacity", 'r') as f:
+        for line in f:
+            capacity.append(line.rstrip().rstrip('\n').split(',')[1])
+    capacity = [float(i) for i in capacity]
+
+    unemployment = []
+    with open("data/unemployment", 'r') as f:
+       for line in f:
+           unemployment.append(map(float, line.rstrip().rstrip('\n').split(' ')))
+    unemployment = [item for sublist in unemployment for item in sublist]
+
+    cpi = []
+    with open("data/cpi", 'r') as f:
+        for line in f:
+            cpi.append(line.rstrip().rstrip('\n').split(',')[1])
+    cpi = map(float, cpi)
+    inflation = [(j - i) / i for i, j in zip(cpi[:-1], cpi[1:])]
+    
+    djia = []
+    with open("data/djia", 'r') as f:
+        for line in f:
+            djia.append(float(line.rstrip()))
+    djia = [(j-i)/i for i,j in zip(djia, djia[1:])]
+    
+    sp = []
+    with open('data/sp', 'r') as f:
+        for line in f:
+            item = line.rstrip().split(',')
+            sp.append(float(item[-2]))
+    sp = [(j-i)/i for i,j in zip(sp, sp[1:])]
+    
+    clusterData = np.array([unemployment, inflation, capacity]).T
+    clusterData = np.array([unemployment, inflation, capacity]).T
+    np.savetxt('data/clusterData.txt', clusterData)
+    
+    fileName = ['microsoft', 'apple', 'att', 'ford', 'sony', 'gap', 'fedex', 'mcdonalds', 'nike',
+    'tiffany', 'homeDepot', 'walmart', 'cocaCola', 'avon', 'oracle', 'ibm', 'intel',
+    'harley-davidson', 'toyota', 'honda', 'boeing', 'jpmorgan', 'boa', 'amgen', 'hermanMiller',
+    'nissan', 'generalElectric', 'nextEra', 'conocoPhillips', 'bakerHughes', 'dukeEnergy', 'chevron']
+    for i in fileName:
+        getInputData(i)
---
+++ svm.py
@@ -0,0 +1,132 @@
+import numpy as np
+from sys import exit
+import matplotlib.pyplot as plt
+from collections import Counter
+from sklearn.decomposition import KernelPCA as pca
+from sklearn import preprocessing, cluster, model_selection, svm
+from sklearn.metrics import f1_score, precision_score, recall_score, precision_recall_curve
+from classifier import Classifier
+
+def f1Score(precision, recall):
+    return 1.0/(1.0 / precision + 1.0 / recall)
+
+def plotPrecisionRecall(precision, recall, thresholds):
+    plt.plot(thresholds, precision[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recall[:-1], "g--", label="Recall")
+    plt.xlabel("Thresholds")
+    plt.legend(loc="upper left")
+    plt.ylim([0, 1])
+    
+    
+
+
+# Object that for svm with clustering 
+class svmStockPred(Classifier):
+    def __init__(self, microDataLoc, clusterNum = 3, macroDataLoc="data/clusterData.txt"):
+        Classifier.__init__(self, microDataLoc, clusterNum, macroDataLoc="data/clusterData.txt")
+
+    def train(self):
+#        cvForDiffClusters = []
+#        for clusterNum in range(1, 5):
+#            train, test, trainLabel, testLabel = self.trainTestSplit(clusterNum)
+#            clf = svm.SVC(C=1000, kernel='rbf') 
+#            yScores = model_selection.cross_val_predict(clf, train[0], trainLabel[0], cv=3,
+#                    method='decision_function')
+#            precision, recall, thresholds = precision_recall_curve(trainLabel[0], yScores)
+#            plotPrecisionRecall(precision, recall, thresholds)
+#            plt.show()
+#            plt.plot(precision[:-1], recall[:-1], 'b--', label='precision_vs_recall')
+#            plt.xlim([0,1])
+#            plt.ylim([0,1])
+#            plt.xlabel("precision")
+#            plt.ylabel("recall")
+#            plt.show()
+#        exit()
+#            cvScore = []
+#            clf = svm.SVC(C=1, kernel='rbf') 
+#            for i in range(clusterNum):
+#                score = model_selection.cross_validate(clf, train[i], trainLabel[i], cv = 5, scoring
+#                     = 'precision', return_train_score = True) 
+#                cvScore.append(score['test_score'].mean()) 
+#            cvForDiffClusters.append(sum(cvScore)/float(len(cvScore)))
+#        clusterNum = cvForDiffClusters.index(max(cvForDiffClusters)) + 1 
+        train, test, trainLabel, testLabel = self.trainTestSplit()
+        clf = [svm.SVC(C=5, kernel='rbf') for i in range(self.clusterNum)]
+        for i in range(self.clusterNum):
+            clf[i].fit(train[i], trainLabel[i])
+        return (clf, test, testLabel) # return test and testLabel to self.test() so no need to
+
+
+# StockPredNoClassification class is a class to classify the stock price direction 
+# without using clustering. The macro data used for clustering now was combined with
+# micro data for classification
+class svmNoCluster(svmStockPred):
+    def __init__(self, microDataLoc):
+        svmStockPred.__init__(self, microDataLoc)
+
+    def prepareData(self):
+        microData = np.loadtxt(self.microDataLoc)
+        macroData = np.loadtxt(self.macroDataLoc)
+#        macroData = preprocessing.scale(macroData)
+        data = np.concatenate((macroData, microData), axis=1)
+        label = data[:, -1]
+        data = data[:, :-1]
+        """
+        nComponents = min(max(np.size(data, 0)/ 30, 1), np.size(data, 1)) # each feature needs 30 samples
+        data = pca(n_components = nComponents, kernel = 'linear').fit_transform(data[:, :-1])
+        """
+        return (data, label)
+
+    def trainTestSplit(self):
+        data, label = self.prepareData()
+        train, test, trainLabel, testLabel= model_selection.train_test_split(data, label, test_size=0.3, random_state=11)
+        return (train, test, trainLabel, testLabel)
+        
+    def train(self):
+        train, test, trainLabel, testLabel = self.trainTestSplit()
+        clf = svm.SVC(C=5, kernel='rbf')
+        clf.fit(train, trainLabel)
+        return (clf, test, testLabel) # return test and testLabel to self.test() so no need to
+
+    def test(self):
+        clf, test, testLabel = self.train()
+        pred = clf.predict(test) 
+#        print pred
+#        print testLabel
+        f1 = f1_score(testLabel, pred)
+        return f1
+
+    def reportResult(self):
+        f1 = self.test()
+        print "Without Clustering, the f1 score is"
+        print f1
+        print '\n'
+        return f1 
+
+            
+if __name__ == "__main__":
+    companies = ['microsoft', 'apple', 'att', 'sony', 'gap', 'fedex', 'mcdonalds', 'nike',
+    'tiffany', 'homeDepot', 'walmart', 'cocaCola', 'avon', 'oracle', 'ibm', 'intel',
+    'harley-davidson', 'toyota', 'boeing', 'jpmorgan', 'boa', 'amgen', 'hermanMiller',
+    'nissan', 'generalElectric', 'nextEra', 'conocoPhillips', 'bakerHughes', 'dukeEnergy', 'chevron']
+#    companies = ['microsoft', 'boeing', 'toyota', 'walmart']
+#   companies = ['toyota']
+    for companyName in companies:
+        print "The stock we are considering now is " + companyName
+        name = "data/" + companyName + "TrainData.txt"
+        # without clustering
+        stock = svmNoCluster(name)
+        stock.reportResult()
+        print '\n'
+   
+        # for the case when cluster = 3
+        stock = svmStockPred(name)
+        stock.reportResult()
+        # Case when 2 clusters
+        stock = svmStockPred(name, clusterNum=2)
+        stock.reportResult()
+
+        # Case when 1 cluster
+        stock = svmStockPred(name, clusterNum=1)
+        stock.reportResult()
+        print '\n'
